01a20c600f9f6961fc36c946babfe5c74c32bde5
Duhan Mo <159794032+DuhanMo@users.noreply.github.com>
2025-08-24 01:30:57 +0900

    Time: 6 ms (80.79%), Space: 45.3 MB (89.88%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.kt
+++ 0383-ransom-note/0383-ransom-note.kt
@@ -1,14 +1,14 @@
 class Solution {
     fun canConstruct(ransomNote: String, magazine: String): Boolean {
-      val map = ('a'..'z').associateWith { 0 }.toMutableMap()
-      magazine.forEach {
-        map[it] = map.getOrDefault(it, 0) + 1
-      }
-      ransomNote.forEach {
-        val remain = map.getOrDefault(it, 0)
-        if (remain - 1 < 0 ) return false
-        map[it] = remain - 1
-      }
-      return true
+        val arr = IntArray(26)
+        magazine.forEach {
+            arr[it - 'a'] += 1
+        }
+        ransomNote.forEach {
+            val remain = arr[it - 'a']
+            if (remain - 1 < 0) return false
+            arr[it - 'a'] = remain - 1
+        }
+    return true
     }
 }
