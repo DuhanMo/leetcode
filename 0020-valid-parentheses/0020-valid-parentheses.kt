68e8e45552ffb7e468c0691f49a5d1ac9dea77f1
Duhan Mo <159794032+DuhanMo@users.noreply.github.com>
2025-08-25 13:43:57 +0900

    Time: 20 ms (33.92%), Space: 46.1 MB (35.96%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.kt
+++ 0020-valid-parentheses/0020-valid-parentheses.kt
@@ -3,28 +3,18 @@ class Solution {
         if (s.length % 2 != 0) return false
         val stack = ArrayDeque<Char>()
         // 문자열 순회
-        // 만약 ( { [ 인 경우 push
-        // ) } ] 인 경우 -> peek이 각각 (, {, [ 인 경우 pop
+        // 만약 ( { [ 인 경우 그 짝을 push
+        // 여는 괄호가 아닌 경우 , 비어있으면 false 꺼내서 짝과 맞지 않을 경우 false
         s.forEach { ch ->
             when {
-                ch == '(' || ch ==  '{' || ch ==  '[' ->  {
-                    stack.add(ch) 
-                }
-                ch == ')' -> {
-                    if (stack.last() == '(')  stack.removeLast()
-                }
-                ch == '}' -> {
-                    if (stack.last() == '{') stack.removeLast()
-                }
-                ch == ']' -> {
-                    if (stack.last() == '[') stack.removeLast()
+                ch == '(' -> stack.addLast(')')
+                ch == '{' -> stack.addLast('}')
+                ch == '[' -> stack.addLast(']')
+                else -> {
+                    if (stack.isEmpty() || stack.removeLast() != ch) return false
                 }
             }
         }
-        return if(stack.size == 0)  {
-            true 
-        } else {
-            false
-        }
+        return stack.isEmpty()
     }
 }
