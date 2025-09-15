515fc4c856f33b21f56f15b929b00f2666952cf0
Duhan Mo <159794032+DuhanMo@users.noreply.github.com>
2025-09-15 13:38:04 +0900

    Time: 0 ms (100%), Space: 50.2 MB (18.99%) - LeetHub

--- 0704-binary-search/0704-binary-search.kt
+++ 0704-binary-search/0704-binary-search.kt
@@ -1,13 +1,12 @@
 class Solution {
     fun search(nums: IntArray, target: Int): Int {
         var left = 0
-        var right = nums.size - 1 // 5
+        var right = nums.size - 1 
         while (left <= right) {
-            // mid = 4
-            val mid = left + right / 2
-            println(mid)
+            
+            val mid = (left + right) / 2
             when {
-                // 3 < 9
+                
                nums[mid] < target -> left = mid + 1 // left = 3
                 nums[mid] > target -> right = mid - 1
                 nums[mid] == target -> return mid
